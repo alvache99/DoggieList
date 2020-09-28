9b0f1721335aadc135a86920d0ca0662c2e2f32d
yourname <alvachelawtiawei@gmail.com>
2020-09-29 06:40:45 +1000

    EmailValidation for both auth

---
+++ Components/AuthScreen.js
@@ -0,0 +1,133 @@
+import React, {useState} from 'react'
+import {StyleSheet,Text,View,TouchableOpacity,TextInput, Button, Image} from 'react-native'
+import {useNavigation} from '@react-navigation/native'
+
+export const AuthScreen = (props) => {
+    const [login,setLogin] = useState(false)
+    const navigation = useNavigation()
+    const [validEmail,setValidEmail] = useState(false)
+    const [validPassword,setValidPassword] = useState(false)
+
+    const validateEmail = (email) => {
+        if(email.indexOf('@')>0 && email.indexOf('.')>0){
+            setValidEmail(true)
+        }else{
+            setValidEmail(false)
+        }
+    }
+
+
+    if(!login){
+        return(
+                   //Login View
+                   <View style={styles.container}>
+                   <View style={{flexDirection:'row', alignContent:'center', justifyContent:'center'}}>
+                       <Image style={{width:40,height:40,marginLeft:10}} source={require('../assets/logo_button.png')}/>
+                       <Text style={styles.title}>DoggieList</Text>
+                   </View>
+                   <View style={styles.mainCont}>
+                       <TextInput 
+                       style={styles.input} 
+                       placeholder="youremail@email.com" 
+                       onChangeText = {(email)=>validateEmail(email)}
+                       />
+                       <TextInput style={styles.input} placeholder="Your Password" secureTextEntry={true}/>
+                   </View>
+                   <View style={styles.buttonView}>
+                       <TouchableOpacity 
+                       style={!validEmail ? styles.submitButtonDisabled : styles.submitButton}
+                       disabled={!validEmail ? true : false}
+                       onPress={()=>{}}
+                       >
+                        <Text style={{fontSize: 30,textAlign:'center',color:'#eeeeee' }}>Login</Text>
+                       </TouchableOpacity>
+                   </View>
+                   <View style={styles.buttonView}>
+                       <TouchableOpacity style={styles.submitButton} onPress={() => {setLogin(true), navigation.setOptions({title: 'Authentication'})}}>
+                               <Text style={{fontSize: 30,textAlign:'center',color:'#eeeeee' }}>Signup</Text>
+                       </TouchableOpacity>
+                   </View>
+               </View>
+        )
+    }
+    else {
+        return(
+        //RegisterView
+        <View style={styles.container}>
+                <View style={{flexDirection:'row', alignContent:'center', justifyContent:'center'}}>
+                    <Image style={{width:40,height:40,marginLeft:10}} source={require('../assets/logo_button.png')}/>
+                    <Text style={styles.title}>Signup</Text>
+                </View>
+                <View style={styles.mainCont}>
+                    <TextInput 
+                    style={styles.input} 
+                    placeholder="youremail@email.com"
+                    onChangeText = {(email)=>validateEmail(email)}
+                    />
+                    <TextInput style={styles.input} placeholder="Your Desired Password" secureTextEntry={true}/>
+                    <TextInput style={styles.input} placeholder="Re-enter Password" secureTextEntry={true}/>
+                </View>
+                <View style={styles.buttonView}>
+                    <TouchableOpacity   
+                       style={!validEmail ? styles.submitButtonDisabled : styles.submitButton}
+                       disabled={!validEmail ? true : false}
+                       onPress={()=>{}}>
+                            <Text style={{fontSize: 30,textAlign:'center',color:'#eeeeee' }}>Confirm</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.buttonView}>
+                    <TouchableOpacity style={styles.submitButton} onPress={()=>setLogin(false)}>
+                            <Text style={{fontSize: 30,textAlign:'center',color:'#eeeeee' }}>Back</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+    container:{
+        flex:1,
+        paddingHorizontal:10,
+        marginTop:50,
+    },
+    title:{
+        fontSize:35,
+        textAlign:'center',
+    },
+    input:{
+        padding:5,
+        borderBottomWidth: 2,
+        borderColor: '#777777',
+        fontSize:20,
+        width:300,
+        marginTop:15,
+        marginLeft:25
+    },
+    mainCont:{
+        flexDirection: 'column',
+        justifyContent:'center',
+        marginTop: 40
+    },
+    submitButton:{
+        borderRadius:10,
+        padding:10,
+        width:150,
+        backgroundColor:"#07E9C0"
+    },
+    submitButtonDisabled:{
+        borderRadius:10,
+        padding:10,
+        width:150,
+        backgroundColor:"#777777"
+    }
+    ,
+    buttonView:{
+        alignContent:'center',
+        flexDirection:'row',
+        justifyContent:'center',
+        marginTop:50,
+    }
+})
---
+++ config/firebase.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB3sskRsewYQTcSK2dU78UZYkirDozs3SY",
+    authDomain: "doggielist-35f4e.firebaseapp.com",
+    databaseURL: "https://doggielist-35f4e.firebaseio.com",
+    projectId: "doggielist-35f4e",
+    storageBucket: "doggielist-35f4e.appspot.com",
+    messagingSenderId: "755782209552",
+    appId: "1:755782209552:web:41d39111c68daff8213a95",
+    measurementId: "G-DQ71NNNLWG"
+  };
